2c04665fc6a623d1321cc2148d601fb7783251b9
Ayumisnice <aymirplaygames2@gmail.com>
2025-10-31 08:46:22 -1100

    Updated Memory system fixed errors

--- Calculator.c
+++ Calculator.c
@@ -1,30 +1,58 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <windows.h>
+#include <math.h>
 
 int i = 0;
 
-float sum = 0;
-float prio = 0;
-float digit = 0;
+double sum = 0;
+double prio = 0;
+double memory =0;
+double digit = 0;
+
+char menuIndex;
 
 char inputs[50];
 char sumOperator = '+';
 char prioOperator = ' ';
 
 char numbers[30];
-char numberIndex = 0;
+int numberIndex = 0;
+
+double a,b,c, d, x1, x2;
+
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void LoadingScreen()
+{
+    printf("\nPlease select a number: ");
+    scanf(" %c", &menuIndex); 
+
+    printf("\n\nLoading...\n");
+
+    for (i = 0; i < 30; i++)
+    {
+            printf("%c", 254);
+            Sleep(20); 
+    }
+    printf("\n");
+}
 
 void Priority()
 {
-    if(prioOperator == '*')
+    if (prioOperator == '*')
     {
         prio *= digit;
     }
-    else if(prioOperator == '/')
+    else if (prioOperator == '/')
     {
-        if (digit ==0){
-            printf("Error\n");
+        if (digit == 0)
+        {
+            printf("Error: Division by zero!\n");
+            return;
         }
         prio /= digit;
     }
@@ -34,68 +62,167 @@ void Priority()
     }
 }
 
-int main()
+void SolveEquation()
 {
-    for(;;)
-    {
-        printf("Please enter a random number :");
-        scanf("%s", inputs);
+    printf("Please Type away your a, b, and c in order : ");
+    
+    scanf("%lf %lf %lf",&a,&b,&c);
 
-        for(i = 0; inputs[i] != '\0'; i++)
-        {
-            char m = inputs[i];
+    LoadingScreen();
 
-            if(m >= '0' && m <= '9')
-            {
-                numbers[numberIndex++] = m;
+       if (a==0) {
+           if (b==0) {
+                if (c==0){
+                   printf("There is infinite solutions \n");
+            }else{
+                  printf("there no solution \n");
             }
-            else
-            {
-                numbers[numberIndex] = '\0';
-                digit = atof(numbers);
-                numberIndex = 0;
+        }else{
+              printf("x= %lf",-c/b);
+        }
+        }else{
+          d=b*b-4*a*c;
+              if (d>0) {
+                   x1=(-b+sqrt(d))/(2*a);
+                   x2=(-b-sqrt(d))    /(2*a);
+                printf("You got Two Solutions x1= %.2lf et x2= %.2lf \n",x1,x2);
 
-                if(m == '*' || m == '/')
-                {
-                    Priority();
-                    prioOperator = m;
-                }
-                else if(m == '+' || m == '-')
-                {
-                    Priority();
+            }else {
+                if (d==0) {
 
-                    if(sumOperator == '+')
-                        sum += prio;
-                    else
-                        sum -= prio;
+                    x1=-b/2*a;
+                    printf("%lf",x1);
 
-                    sumOperator = m;
-                    prioOperator = ' ';
-                }
+                } else {
+                     printf("no solutions in R \n");
             }
         }
-
-        numbers[numberIndex] = '\0';
-        digit = atof(numbers);
-
-        Priority();
-
-        if(sumOperator == '+')
-        {
-            sum += prio;
-        }else sum -= prio;
-
-        printf("The result is : %f \n", sum);
-
-        sum=0;
-        prio=0;
-        
-        sumOperator = '+';
-        prioOperator = ' ';
-
-        memset(inputs, 0, sizeof(inputs));
-        memset(numbers, 0, sizeof(numbers));
-        numberIndex = 0;
-        continue;
     }
+}
+
+
+int main()
+{
+    while (1)
+    {
+    setColor(5);
+    printf("===============================================================================================\n");
+    printf("                                    CASIO CALCULATOR                                           \n");
+    printf("===============================================================================================\n");
+    printf("1. GUIDE\n");
+    printf("2. CALCULATION\n");
+    printf("3. EQUATIONS\n");
+    printf("4. RESET\n");
+    printf("5. QUIT\n");
+    printf("6. CREDITS\n");
+    printf("===============================================================================================\n");
+
+        LoadingScreen();
+
+        if (menuIndex == '5')
+        {
+            printf("Goodbye!\n");
+            break;
+        }
+        else if (menuIndex == '4')
+        {
+            sum = prio = digit = 0;
+            sumOperator = '+';
+            prioOperator = ' ';
+            printf("Memory Reset!\n");
+            continue;
+        }
+        else if (menuIndex == '3')
+        {
+           SolveEquation();
+            continue;
+        }
+        else if (menuIndex == '2')
+        {
+            printf("calculation start!!\n");
+        }
+        else if (menuIndex == '1')
+        {
+            printf("Guide:\n");
+            continue;
+        }
+        else
+        {
+            printf("Please select a valid option .\n");
+            continue;
+        }
+
+        while (1)
+        {
+            printf("\nPlease enter a random expression : ");
+            scanf("%s", inputs);
+
+            sum = 0;
+            prio = 0;
+            sumOperator = '+';
+            prioOperator = ' ';
+            numberIndex = 0;
+
+            for (i = 0; inputs[i] != '\0'; i++)
+            {
+                char m = inputs[i];
+
+                if (m >= '0' && m <= '9' || m == '.')
+                {
+                    numbers[numberIndex++] = m;
+                }else if(m == 'm')
+                {
+                    snprintf(numbers, sizeof(numbers), "%g", memory);
+                }
+                else if (m == '+' || m == '-' || m == '*' || m == '/')
+                {
+                    numbers[numberIndex] = '\0';
+                    digit = atof(numbers);
+                    numberIndex = 0;
+
+                    if (m == '*' || m == '/') 
+                    {
+                        Priority();
+                        prioOperator = m;
+                    }
+                    else if (m == '+' || m == '-')
+                    {
+                        Priority();
+
+                        if (sumOperator == '+' )
+                            sum += prio;
+                        else
+                            sum -= prio;
+
+                        sumOperator = m;
+                        prioOperator = ' ';
+                    }
+                }else
+                {
+                    printf("ERROR CHARACTER NOT IDENTIFIED PLEASE TRY AGAIN \n");
+                    continue;
+                }
+            }
+
+            numbers[numberIndex] = '\0';
+            digit = atof(numbers);
+            Priority();
+
+            if (sumOperator == '+')
+                sum += prio;
+            else
+                sum -= prio;
+
+            printf("The result is: %lf\n", sum);
+            memory = sum;
+            printf("The Memory is : %lf", memory);
+
+            sumOperator = '+';
+            prioOperator = ' ';
+
+            memset(inputs, 0, sizeof(inputs));
+            memset(numbers, 0, sizeof(numbers));
+        }
+    }
+    return 0;
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,64 +1,228 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+#include <math.h>
 
 int i = 0;
 
-float sum;
+double sum = 0;
+double prio = 0;
+double memory =0;
+double digit = 0;
 
-float digits[9];
-char digitIndex =0;
+char menuIndex;
 
 char inputs[50];
+char sumOperator = '+';
+char prioOperator = ' ';
+
 char numbers[30];
-char numberIndex;
+int numberIndex = 0;
 
-void Addition()
+double a,b,c, d, x1, x2;
+
+void setColor(int color) {
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+}
+
+void LoadingScreen()
 {
-    printf("You are performing addition \n");
+    printf("\nPlease select a number: ");
+    scanf(" %c", &menuIndex); 
 
-    if(digitIndex == 0)
-    {
-        digits[digitIndex] = atof(numbers);
-        printf("%f \n", digits[digitIndex]);
-        digitIndex++;
-    }else if(digitIndex == 1)
-    {
-        printf("The digit index is 1 \n");
-        digits[digitIndex] = atof(numbers);
-        printf("%f \n", digits[digitIndex]);
-        sum = digits[0] + digits[1];
-        digitIndex = 0;
+    printf("\n\nLoading...\n");
 
-        digits[0] = '\0';
-        digits[1] = '\0';
+    for (i = 0; i < 30; i++)
+    {
+            printf("%c", 254);
+            Sleep(20); 
+    }
+    printf("\n");
+}
+
+void Priority()
+{
+    if (prioOperator == '*')
+    {
+        prio *= digit;
+    }
+    else if (prioOperator == '/')
+    {
+        if (digit == 0)
+        {
+            printf("Error: Division by zero!\n");
+            return;
+        }
+        prio /= digit;
+    }
+    else
+    {
+        prio = digit;
     }
 }
 
-int main()
+void SolveEquation()
 {
-    printf("Please enter a random number :");
-    scanf("%s", &inputs);
+    printf("Please Type away your a, b, and c in order : ");
+    
+    scanf("%lf %lf %lf",&a,&b,&c);
 
-    for(i; inputs[i] != '\0'; i++)
-    {
-        char m = inputs[i];
+    LoadingScreen();
 
-        if(m >= '0' && m <= '9')
-        {
-            numbers[numberIndex++] = m;
+       if (a==0) {
+           if (b==0) {
+                if (c==0){
+                   printf("There is infinite solutions \n");
+            }else{
+                  printf("there no solution \n");
+            }
+        }else{
+              printf("x= %lf",-c/b);
+        }
+        }else{
+          d=b*b-4*a*c;
+              if (d>0) {
+                   x1=(-b+sqrt(d))/(2*a);
+                   x2=(-b-sqrt(d))    /(2*a);
+                printf("You got Two Solutions x1= %.2lf et x2= %.2lf \n",x1,x2);
 
-            printf("You have %s \n", numbers);
-        }else if(m == '+' || m == '-')
-        {
-            numbers[numberIndex] = '\0';
-            numberIndex = 0;
+            }else {
+                if (d==0) {
 
-            switch(m)
-            {
-                case '+' : Addition(); continue;
+                    x1=-b/2*a;
+                    printf("%lf",x1);
+
+                } else {
+                     printf("no solutions in R \n");
             }
         }
-    }  
-    
-    printf("The result is : %f", sum);
+    }
+}
+
+
+int main()
+{
+    while (1)
+    {
+    setColor(5);
+    printf("===============================================================================================\n");
+    printf("                                    CASIO CALCULATOR                                           \n");
+    printf("===============================================================================================\n");
+    printf("1. GUIDE\n");
+    printf("2. CALCULATION\n");
+    printf("3. EQUATIONS\n");
+    printf("4. RESET\n");
+    printf("5. QUIT\n");
+    printf("6. CREDITS\n");
+    printf("===============================================================================================\n");
+
+        LoadingScreen();
+
+        if (menuIndex == '5')
+        {
+            printf("Goodbye!\n");
+            break;
+        }
+        else if (menuIndex == '4')
+        {
+            sum = prio = digit = 0;
+            sumOperator = '+';
+            prioOperator = ' ';
+            printf("Memory Reset!\n");
+            continue;
+        }
+        else if (menuIndex == '3')
+        {
+           SolveEquation();
+            continue;
+        }
+        else if (menuIndex == '2')
+        {
+            printf("calculation start!!\n");
+        }
+        else if (menuIndex == '1')
+        {
+            printf("Guide:\n");
+            continue;
+        }
+        else
+        {
+            printf("Please select a valid option .\n");
+            continue;
+        }
+
+        while (1)
+        {
+            printf("\nPlease enter a random expression : ");
+            scanf("%s", inputs);
+
+            sum = 0;
+            prio = 0;
+            sumOperator = '+';
+            prioOperator = ' ';
+            numberIndex = 0;
+
+            for (i = 0; inputs[i] != '\0'; i++)
+            {
+                char m = inputs[i];
+
+                if (m >= '0' && m <= '9' || m == '.')
+                {
+                    numbers[numberIndex++] = m;
+                }else if(m == 'm')
+                {
+                    snprintf(numbers, sizeof(numbers), "%g", memory);
+                }
+                else if (m == '+' || m == '-' || m == '*' || m == '/')
+                {
+                    numbers[numberIndex] = '\0';
+                    digit = atof(numbers);
+                    numberIndex = 0;
+
+                    if (m == '*' || m == '/') 
+                    {
+                        Priority();
+                        prioOperator = m;
+                    }
+                    else if (m == '+' || m == '-')
+                    {
+                        Priority();
+
+                        if (sumOperator == '+' )
+                            sum += prio;
+                        else
+                            sum -= prio;
+
+                        sumOperator = m;
+                        prioOperator = ' ';
+                    }
+                }else
+                {
+                    printf("ERROR CHARACTER NOT IDENTIFIED PLEASE TRY AGAIN \n");
+                    continue;
+                }
+            }
+
+            numbers[numberIndex] = '\0';
+            digit = atof(numbers);
+            Priority();
+
+            if (sumOperator == '+')
+                sum += prio;
+            else
+                sum -= prio;
+
+            printf("The result is: %lf\n", sum);
+            memory = sum;
+            printf("The Memory is : %lf", memory);
+
+            sumOperator = '+';
+            prioOperator = ' ';
+
+            memset(inputs, 0, sizeof(inputs));
+            memset(numbers, 0, sizeof(numbers));
+        }
+    }
+    return 0;
 }
